fe8bb0360d0e99f6c7e79d7875bcfa3c03560cd4
Mukeli3 <mukelicynthia20@gmail.com>
2024-08-24 00:59:53 +0000

    MRU

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+MRUCache
+"""
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    """
+    def __init__(self):
+        """
+        """
+        super().__init__()
+        self.cache_data = {}
+
+    def put(self, key, item):
+        """
+        """
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+
+            else:
+                if len(self.cache_data) >+ BaseCaching.MAX_ITEMS:
+                    mru_key = next(reversed(self.cache_data))
+                    self.cache_data.pop(mru_key)
+                    print("DISCARD:", mru_key)
+
+            self.cache_data[key] = item
+    def get(self, key):
+        """
+        Gets an item from the cache by key and if key doesn't exist or is
+        None, returns None
+        """
+        if key is None:
+            return None
+        return self.cache_data.get(key)
+ 
