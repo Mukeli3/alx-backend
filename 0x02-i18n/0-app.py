a9c760b66cb8f0628be957cfb6dedc95faebbaa2
Mukeli3 <mukelicynthia20@gmail.com>
2024-08-27 21:30:37 +0000

    documentation

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
+"""
+This module defines a basic flask app
+"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """
+    function to render index.html template
+    """
     render_template('index.html')
 
 if __name__ == "__main__":
