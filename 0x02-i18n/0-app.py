e37eb10e11817817f2515270484fdb55c9f3f536
Mukeli3 <mukelicynthia20@gmail.com>
2024-08-27 21:32:38 +0000

    pycodestyle fixed

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,12 +6,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     """
     function to render index.html template
     """
     render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
