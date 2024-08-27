4ee354158c1231ad513fea6d28d694da613d35d3
Mukeli3 <mukelicynthia20@gmail.com>
2024-08-27 22:24:55 +0000

    babel-ed

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -6,12 +6,27 @@ from flask import request
 from flask_babel import Babel
 
 
+class Config:
+    """
+    Class configuration, language and timezone
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
 @babel.localeselector
 def get_locale() -> str:
     """
     function gets language code for a web page
     """
-    return request.accept_languages.best_match(['fr', 'en'])
+    return request.accept_languages.best_match([LANGUAGES])
 
 
 @app.route('/')
