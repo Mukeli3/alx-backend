4554537da05fc8ffdebebe0074dd966338f9ec8a
Mukeli3 <mukelicynthia20@gmail.com>
2024-08-27 22:54:26 +0000

    typo

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -17,7 +17,7 @@ class Config:
 
 app = Flask(__name__)
 app.config.from_object(Config)
-pp.url_map.strict_slashes = False
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
@@ -26,7 +26,7 @@ def get_locale() -> str:
     """
     function gets language code for a web page
     """
-    return request.accept_languages.best_match(['LANGUAGES'])
+    return request.accept_languages.best_match([app.config'LANGUAGES'])
 
 
 @app.route('/')
