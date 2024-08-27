62992fb9a44a3e80b2f2db4bd98b1dc0d9a502d8
Mukeli3 <mukelicynthia20@gmail.com>
2024-08-27 21:46:21 +0000

    return

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -12,7 +12,7 @@ def index():
     """
     function to render index.html template
     """
-    render_template('0-index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == "__main__":
