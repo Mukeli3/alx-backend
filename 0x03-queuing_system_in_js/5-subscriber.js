f14260585653cf86fb9edbc65c65cda29bd47498
Mukeli3 <mukelicynthia20@gmail.com>
2024-11-04 15:49:58 +0000

    correct name and subscriber

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -2,23 +2,22 @@
 import { createClient } from 'redis';
 
 const client = createClient();
+const EXIT_MSG = 'KILL_SERVER';
 
 client.on('error', (err) => {
   console.log('Redis client not connected to the server:', err.toString());
 });
 
-const publishMsg = (message, time) => {
-  setTimeout(() => {
-    console.log(`About to send ${message}`);
-    client.publish('holberton school channel', message);
-  }, time);
-};
-
 client.on('connect', () => {
   console.log('Redis client connected to the server');
 });
 
-publishMsg('Holberton Student #1 starts course', 100);
-publishMsg('Holberton Student #2 starts course', 200);
-publishMsg('KILL_SERVER', 300);
-publishMsg('Holberton Student #3 starts course', 400);
+client.subscribe('holberton school channel');
+
+client.on('message', (_err, msg) => {
+  console.log(msg);
+  if (msg === EXIT_MSG) {
+    client.unsubscribe();
+    client.quit();
+  }
+});
